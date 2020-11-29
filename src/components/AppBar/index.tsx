8193f963d6dab68edf42a4bf6e084a1b70d05b33
Guilherme Paes <guilhermep98@gmail.com>
2020-11-29 14:18:36 -0300

    Fixed a bug

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,9 +1,6 @@
 import React, {ReactNode} from "react";
 import "./styles.css";
 
-/* Basically the 'top' value does nothing special at all. 
-The only reason I  kept it was for readability purposes */
-
 interface IAppBar { 
   children?: ReactNode
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,27 +1,27 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useLayoutEffect } from "react";
 import "./styles.css";
 
 export default function Main(props: any) {
   const refContainer = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(Number);
-  const [currentHeight, setCurrentHeight] = useState(Number);
   const [shouldUpdateHeight, setShouldUpdateHeight] = useState(false);
   const [styleObject, setStyleObject] = useState({});
   const [mouseY, setMouseY] = useState(0);
 
-  const handleMouseDown = (event: any) => {setShouldUpdateHeight(true); setCurrentHeight(event.pageY)};
-  const handleMouseUp = () => setShouldUpdateHeight(false);
+  const handleMouseDown = (event: any) => setShouldUpdateHeight(true);
+  const handleMouseUp = () => setShouldUpdateHeight(false); 
   // TODO find the correct type for event
   const handleMouseMove = (event: any) => {
-    setMouseY(event.movementY)
+    
     if(shouldUpdateHeight){
-      console.log('offsset', mouseY)
-      setCurrentHeight(prevHeight => prevHeight - mouseY)
-      setStyleObject({height: currentHeight })
+      setMouseY(event.movementY)
+      console.log('offset', mouseY)
+      setHeight(prevHeight => prevHeight - mouseY)
+      setStyleObject({height: height })
     } 
   }
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setHeight(refContainer.current!.clientHeight);
     console.log(refContainer.current!);
   }, []);
