f4ae4bcbb86e15ad5233fb320b74e16982942607
Guilherme Paes <guilhermep98@gmail.com>
2020-11-28 23:15:15 -0300

    Height Update Bug

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,24 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./styles.css";
+
 export default function Main(props: any) {
   const refContainer = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState(Number);
   const [currentHeight, setCurrentHeight] = useState(Number);
   const [shouldUpdateHeight, setShouldUpdateHeight] = useState(false);
   const [styleObject, setStyleObject] = useState({});
-  const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
+
+  const handleMouseDown = () => setShouldUpdateHeight(true);
+  const handleMouseUp = () => setShouldUpdateHeight(false);
+  // TODO find the correct type for event
+  const handleMouseMove = (event: any) => {
+    if(shouldUpdateHeight){
+      setMouseY(event.clientY)
+      setHeight(mouseY) //FIX THIS
+      setStyleObject({height: height})
+    } 
+  }
 
   useEffect(() => {
     setHeight(refContainer.current!.clientHeight);
@@ -16,26 +27,15 @@ export default function Main(props: any) {
 
   return (
     <div className="box">
-      <div
-        ref={refContainer}
-        style={styleObject}
-        onMouseDown={() => {
-          setShouldUpdateHeight(true);
-        }}
-        onMouseUp={() => {
-          setShouldUpdateHeight(false);
-        }}
-        onMouseMove={(e) => {
-          if (shouldUpdateHeight) {
-            setMouseX(e.clientX);
-            setMouseY(e.clientY);
-          }
-        }}
-        className="resizable-stuff"
-      >
-        <div className="stuff"><p style={{alignSelf: 'center', justifySelf: 'center'}}>PUTA QUE PARIU</p></div>
-        Update Height?: {shouldUpdateHeight.toString()}
-        Coordinates: {mouseX} {mouseY}
+      <div ref={refContainer} style={styleObject} className="resizable-stuff">
+        <div className="stuff">
+          <p onMouseDownCapture={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} style={{ alignSelf: "center", justifySelf: "center", cursor: "ns-resize" }}>
+            PUTA QUE PARIU
+          </p>
+        </div>
+       <p>Initial Height: {height}</p>
+       <p>Update Height?: {shouldUpdateHeight.toString()}</p>
+        <p>Coordinates: {mouseY}</p>
       </div>
     </div>
   );
