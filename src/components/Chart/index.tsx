8a6f0ab2612dd74d55f8033b9d3e82daff7a5524
Guilherme Paes <guilhermep98@gmail.com>
2020-12-06 02:36:18 -0300

    changed the appBar unit and added highcharts

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -1,37 +1,90 @@
 import React from "react";
-import {
-  VictoryLine,
-  VictoryContainer,
-  VictoryChart,
-  VictoryTheme,
-} from "victory";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 import "./styles.css";
-const data = [{}];
+const data = [
+  { quarter: 0, earnings: 1.4 },
+  { quarter: 0, earnings: 1.4 },
+  { quarter: 1, earnings: 1.5 },
+  { quarter: 1, earnings: 1.5 },
+];
+const data2 = [
+  { quarter: 0, earnings: 0.1 },
+  { quarter: 0, earnings: 2.2 },
+  { quarter: 1, earnings: 1.5 },
+  { quarter: 1, earnings: 1.5 },
+];
+const data3 = [
+  { quarter: 0, earnings: 0.1 },
+  { quarter: 0, earnings: 1.1 },
+  { quarter: 1, earnings: 1.5 },
+  { quarter: 1, earnings: 1.2 },
+];
 
-interface IChart{
+interface IChart {
   mouseUp: any;
   mouseMove: any;
-  data: JSON[]
+  data: JSON[];
 }
 
-export default function Chart(props: any) {
+const options: Highcharts.Options = {
+  title: {
+    text: "",
+  },
+  chart:{
+    height: 320,
+    className: "chart"
+  },
+  xAxis: {
+    categories: [],
+    tickmarkPlacement: "on",
+  },
+  yAxis: {
+    labels: { enabled: false },
+    title:{text: null}
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false,
+      },
+      marker: {
+        symbol: "circle",
+        radius: 7,
+        lineWidth: 1,
+      },
+    },
+  },
+  credits: {
+    enabled: false,
+  },
+  legend: {
+    layout: "vertical",
+    itemStyle: {
+      textTransform: "capitalize",
+    },
+    align: "right",
+    itemDistance: 100,
+    verticalAlign: "top",
+  },
+  series: [
+    {
+      type: "line",
+      data: [1, 2, 3],
+    },
+    {
+      type: "line",
+      data: [0, 1, 5],
+    },
+  ],
+};
+
+export default function Chart(props: IChart) {
   return (
-    <VictoryChart
-      theme={VictoryTheme.material}
-      height={300}
-      width={800}
-      events={[
-        {
-          childName: "all",
-          target: "parent",
-          eventHandlers: {
-            onMouseUp: () => props.mouseUp,
-            onMouseMove: () => props.mouseMove,
-          },
-        },
-      ]}
-    >
-      <VictoryLine data={data} x="quarter" y="earnings" />
-    </VictoryChart>
+    <div>
+    <HighchartsReact reflow={false} highcharts={Highcharts} options={options} />
+    </div>
+    
   );
 }
